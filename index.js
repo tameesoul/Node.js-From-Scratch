c06e567143bcb1ceacff348a87a82d069c6ddf7a
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-07-16 10:25:16 +0300

    Exercises  02

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 
-
+/// Exercises – 01
 function CircleArea(r)
 {
     var A = Math.PI*(r*r)
     console.log(A);
 }
 
+//Exercises – 02
 
-CircleArea(5);
+var x = 5;
+var y = 6;
+var temp;
+temp = x;
+x = y;
+y = temp;
+//console.log( "x:",x ,"y:",y);
+
+
+function FahrenheitTo(f)
+{
+    var c = (f-32)*5/9;
+    console.log(c);
+}
+
+FahrenheitTo(86);
+
+
+
